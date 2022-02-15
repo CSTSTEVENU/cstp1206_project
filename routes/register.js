01bed13ea7d5d1ff6cc1222b109e6b7cac283a11
jingjinjessica <jingjin496@gmail.com>
2022-02-14 21:11:28 -0800

    register router

--- routes/register.js
+++ routes/register.js
@@ -1,28 +1,48 @@
 var express = require('express');
 var router = express.Router();
 const bcrypt = require('bcrypt')
-const users = []
+const { PrismaClient } = require( '@prisma/client');
+const prisma = new PrismaClient()
 
 router.get('/', function(req, res) {
   console.info("I'm in the Register Router");
   res.render('register.jade');
 });
 
-router.post('/register', async (req, res) => {
+router.post('/', async (req, res) => {
   try{
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    users.push({
-      id: Date.now().toString(),
-      name: req.body.name,
+    if (req.body.pwd !== req.body.cmpwd){
+      res.render("register.jade", {message:"The passwords are not match."});
+      return;
+    }
+
+    const user = await prisma.user.findFirst({
+      where: {
+        OR:[
+          {name: req.body.username},
+          {password: req.body.pwd}
+        ]
+      }
+    });
+
+    if (user){
+      res.render("register.jade", {message:"User name or email is already registered, please try use different user name/email."});
+      return;
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.pwd, 10)
+    var newUser = await prisma.user.create({data:{
+      name: req.body.username,
       email: req.body.email,
       password: hashedPassword
-    })
-    res.redirect('/login')
-  }catch
+    }});
+    res.redirect('/login');
+  }catch(error)
   {
-    res.redirect('/register')
+    console.error(error);
+    res.redirect('/register');
   }
-  console.log(users)
+  console.log(newUser)
 })
 
 module.exports = router;
