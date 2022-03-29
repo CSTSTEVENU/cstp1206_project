5e79d850f0a2ebe283ac7648ad582e6d94929ece
jingjinjessica <jingjin496@gmail.com>
2022-03-29 00:36:23 -0700

    fix bug

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'production' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
